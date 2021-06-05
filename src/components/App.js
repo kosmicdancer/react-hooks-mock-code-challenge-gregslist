a693c0ca1876e01ffbf09568cdf380fba6b47293
Kyoko <kyokoconnect@gmail.com>
2021-06-05 15:33:49 -0400

    advanced deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,20 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
-  const [listings, setListings] = useState([]);
-  const [search, setSearch] = useState("");
-
-  useEffect(() => {
-    fetch("http://localhost:6001/listings")
-      .then((r) => r.json())
-      .then(setListings);
-  }, []);
-
-  function handleRemoveListing(id) {
-    const newListings = listings.filter((listing) => listing.id !== id);
-    setListings(newListings);
-  }
-
-  const displayedListings = listings.filter((listing) =>
-    listing.description.toLowerCase().includes(search.toLowerCase())
-  );
+    const [search, setSearch] = useState("");
+  
+    function handleSearch(newSearch) {
+      setSearch(newSearch);
+    }
   
   return (
     <div className="app">
-      <Header onSearch={setSearch} />
-      <ListingsContainer
-      listings={displayedListings}
-        onRemoveListing={handleRemoveListing} />
+      <Header onSearch={handleSearch} />
+      <ListingsContainer search={search}/>
     </div>
   );
 }
 
-export default App;
+export default App
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,20 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ListingCard from "./ListingCard";
+import NewListingForm from "./NewListingForm";
 
 
+function ListingsContainer({search}) {
+  const [listings, setListings] = useState([]);
+  const [sortBy, setSortBy] = useState("id");
 
-function ListingsContainer({ listings, onRemoveListing }) {
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then((r) => r.json())
+      .then((listings) => setListings(listings));
+  }, []);
+
+  function handleDeleteListing(id) {
+    const updatedListingsArray = listings.filter(
+      (listing) => listing.id !== id
+    );
+    setListings(updatedListingsArray);
+  }
+
+  function handleAddListing(newListing) {
+    const updatedListingsArray = [newListing, ...listings];
+    setListings(updatedListingsArray);
+  }
+
+  const listingCards = listings
+    .filter((listing) =>
+      listing.description.toLowerCase().includes(search.toLowerCase())
+    )
+    .sort((listingA, listingB) => {
+      if (sortBy === "id") {
+        return listingA.id - listingB.id;
+      } else {
+        return listingA.location.localeCompare(listingB.location);
+      }
+    })
+    .map((listingObj) => (
+      <ListingCard
+        key={listingObj.id}
+        listing={listingObj}
+        onDeleteListing={handleDeleteListing}
+      />
+    ));
+  
   return (
     <main>
-      <ul className="cards">
-        {listings.map((listing) => (
-          <ListingCard
-            key={listing.id}
-            listing={listing}
-            onRemoveListing={onRemoveListing}
-          />
-        ))}
-      </ul>
+      <NewListingForm onAddListing={handleAddListing} />
+      <button onClick={() => setSortBy("id")}>Sort By Default</button>
+      <button onClick={() => setSortBy("location")}>Sort By Location</button>
+      <ul className="cards">{listingCards}</ul>
     </main>
   );
 }
